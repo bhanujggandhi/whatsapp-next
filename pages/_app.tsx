8da296d3b8d926b48fce2a51d418ec81aceec606
bhanujggandhi <gandhibhanuj@gmail.com>
2021-03-29 23:57:13 +0530

    feat: user store in db

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
-import "../styles/globals.css";
+import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 import Login from "./login";
 import Loading from "components/Loading.jsx";
 import { auth, db } from "../firebase";
+
+import "../styles/globals.css";
 
 function MyApp({
   Component,
@@ -13,6 +17,17 @@ function MyApp({
   pageProps: any;
 }) {
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    db.collection("users").doc(user?.uid).set(
+      {
+        email: user?.email,
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        photoURL: user?.photoURL,
+      },
+      { merge: true }
+    );
+  }, []);
 
   if (loading) return <Loading />;
 
